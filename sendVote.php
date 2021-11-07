eb0d3d912b7c2f667e9d064be249a6afe4823b0f
Alex M <alex@obliv1on.com>
2021-11-07 16:01:13 +0100

    Fix SQL vuln and add additional checks (#5)
    * Add Auth check to vote and sendVote
    
    * Revert "Add Auth check to vote and sendVote"
    
    This reverts commit e3864cb6f8f6362e8470c43f85fc2bf18a43cad5.
    
    * Add Auth check
    
    * Add Vote Check and look if poll already concluded
    
    * Fix faulty variable reference
    
    * Add value sanitation to SQL query

--- sendVote.php
+++ sendVote.php
@@ -4,21 +4,38 @@
   ini_set('display_startup_errors', 1);
   ini_set('max_execution_time', 120);
   error_reporting(E_ALL);
-  session_start();
+  require './login.php';
   require_once './assets/config.php';
   $psql = pg_connect("$db->host $db->port $db->name $db->credentials");
-  echo "<script>console.log('User Info retrieved from Verifier API:')</script>";
-  echo "<script>console.log('" . json_encode($_SESSION['api_user']) . "')</script>";
-  echo "<script>console.log('User info retrieved from Discord OAuth2 grant:')</script>";
-  echo "<script>console.log('" . json_encode($_SESSION['oauth_user']) . "')</script>";
+  
+  if (empty($_SESSION['access_token'])) {
+    header('Location: error.php?eCode=auth_err&eDesc=Not Authenticated');
+    die();
+  } else if ($_SESSION['api_user']->hasMember === false) {
+    header('Location: error.php?eCode=no_role');
+    die();
+  }
+  
+  $already_voted = voteStatus($psql, $_SESSION['api_user']->id);
+  if (empty($already_voted)) {
+    echo "<script>console.log('User has not voted yet!');</script>";
+  } else {
+    header('Location: error.php?eCode=already_voted');
+    die();
+  }
+
+  if ($duedate < date('Y-m-d')) {
+    header('Location: error.php?eCode=concluded');
+  }
 
   $edit_code = generateCode(16);
   $votedList = [];
   foreach ($_POST as $nomineeId) {
     if (!in_array($nomineeId, $votedList)) {
+      $nomineeId = pg_escape_string($nomineeId);
       $insert = "INSERT INTO votes (nominee, edit_code) VALUES ('{$nomineeId}', '{$edit_code}')";
       $res = pg_query($psql, $insert);
-      array_push($voted, $nomineeId);
+      array_push($votedList, $nomineeId);
     } else {
       echo "<script>console.log('Duplicate vote detected, omitting.')</script>";
     }
